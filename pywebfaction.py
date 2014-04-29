890b42e8f6e9fcbe7091037fef4e959173c8c76f
Dominic Rodger <dominicrodger+github@gmail.com>
2014-04-29 17:48:42 +0100

    Add a function for generating a mailbox name from an email address

--- pywebfaction.py
+++ pywebfaction.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import string
 import xmlrpclib
 
 
@@ -30,10 +31,40 @@ class WebFactionAPI(object):
 
         return [Email(r) for r in response]
 
+    @staticmethod
+    def email_to_mailbox_name(email_address):
+        if not email_address:
+            raise ValueError("E-mail addresses cannot be empty.")
+
+        email_address = email_address.lower()
+        valid_characters = string.ascii_letters + string.digits + '_'
+
+        def is_valid_character(c):
+            return c in valid_characters
+
+        def make_valid(c):
+            if is_valid_character(c):
+                return c
+
+            if c == '@':
+                return '_'
+
+            return ''
+
+        joined_up = ''.join([make_valid(c) for c in email_address])
+
+        if joined_up == '_' * len(joined_up):
+            raise ValueError(
+                "Mailbox names must contain at least one valid "
+                "character."
+            )
+
+        return joined_up
+
     def create_email(self, email_address):
-        # Need to generate a mailbox name (may only contain lowercase
-        # letters, numbers and _)
-        mailbox = 'foobar9871'
+        # Mailbox names may only contain lowercase letters, numbers
+        # and _.
+        mailbox = WebFactionAPI.email_to_mailbox_name(email_address)
         mailbox_result = self.server.create_mailbox(
             self.session_id,
             mailbox
--- test_pywebfaction.py
+++ test_pywebfaction.py
@@ -229,7 +229,7 @@ def test_create_email():
             {
                 'email_address': 'foo@example.net',
                 'id': 42,
-                'targets': 'foobar9871',
+                'targets': 'foo_exampleorg',
             },
         )
     )
@@ -238,5 +238,35 @@ def test_create_email():
     response = api.create_email('foo@example.org')
 
     assert response.password == 'password1'
-    assert response.mailbox == 'foobar9871'
+    assert response.mailbox == 'foo_exampleorg'
     assert response.email_id == 42
+
+
+def test_email_to_mailbox_all_invalid():
+    with pytest.raises(ValueError):
+        WebFactionAPI.email_to_mailbox_name('*+@')
+
+
+def test_email_to_mailbox_empty_string():
+    with pytest.raises(ValueError):
+        WebFactionAPI.email_to_mailbox_name('')
+
+
+def test_email_to_mailbox_simple():
+    result = WebFactionAPI.email_to_mailbox_name('foo@example.org')
+    assert result == 'foo_exampleorg'
+
+
+def test_email_to_mailbox_invalid_stripped():
+    result = WebFactionAPI.email_to_mailbox_name('foo+b@example.org')
+    assert result == 'foob_exampleorg'
+
+
+def test_email_to_mailbox_uppercase_lowercased():
+    result = WebFactionAPI.email_to_mailbox_name('FOO@example.org')
+    assert result == 'foo_exampleorg'
+
+
+def test_email_to_mailbox_underscores_retained():
+    result = WebFactionAPI.email_to_mailbox_name('foo_b@example.org')
+    assert result == 'foo_b_exampleorg'
